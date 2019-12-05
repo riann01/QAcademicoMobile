9c451487ae86f72b13b9c3f404b3ac1e1963ed66
riann01 <rianinacio@gmail.com>
2019-12-05 09:39:54 -0300

    Instaladas dependências para sombras no Android

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,32 +1,29 @@
 import React from 'react';
-import { StyleSheet, ImageBackground, Image, ScrollView } from 'react-native';
+import { StyleSheet, Image, ScrollView } from 'react-native';
 import { mapping, light as lightTheme } from '@eva-design/eva';
 import { ApplicationProvider, Layout, Card, Text, Button, Input, Avatar } from 'react-native-ui-kitten';
-
-const Header = () => (
-    <CardHeader
-      title='Title'
-      description='Description'
-    />
-  );
 
 class Dashboard extends React.Component {
     render() {
         return (
             <React.Fragment>
-            <ApplicationProvider mapping={mapping} theme={lightTheme}>
-                <Card header={Header}>
-                    <Avatar
-                    style={styles.item}
-                    size='giant'
-                    source={{ uri: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330' }}
-                    />
-                    <Layout style={{flexDirection: 'column', alignContent: 'center', alignItems: 'center'}}>
-                        <Text style={{fontWeight: 'bold'}}>Nome do Aluno</Text>
-                        <Text style={{fontWeight: 'bold'}}>20191070130000</Text>
+                <ApplicationProvider mapping={mapping} theme={lightTheme}>
+                    <Layout style={{alignItems: 'center', flex: 1}}>
+                        <Layout style={styles.containerData}>
+                            <Image
+                            source={require('../img/avatar-icon-images-4.jpg')}
+                            style={{marginTop: '5%', marginBottom: '5%', height: '150%'}}
+                            resizeMode="contain"
+                            />
+                            <Text style={{fontWeight: 'bold'}}>Nome do Aluno</Text>
+                            <Text style={{fontWeight: 'bold'}}>20191070130000</Text>
+                        </Layout>
+                        <Layout style={{flexDirection: 'row'}}>
+                            <Layout style={styles.containerOpcao}></Layout>   
+                            <Layout style={styles.containerOpcao}></Layout>   
+                        </Layout>
                     </Layout>
-                </Card>
-            </ApplicationProvider>
+                </ApplicationProvider>
             </React.Fragment>
         );
     }    
@@ -37,6 +34,27 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignContent: 'center',
         alignItems: 'center',
+    },
+    containerData: {
+        flexDirection: 'column',
+        alignContent: 'center',
+        alignItems: 'center',
+        shadowColor: '#000000',
+        borderTopColor: '#3B8C2E',
+        borderTopWidth: 7,
+        borderTopRightRadius: 7,
+        borderTopLeftRadius: 7,
+        marginTop: '10%',
+        width: '80%',
+        height: '15%'
+    },
+    containerOpcao: {
+        borderTopColor: '#3B8C2E',
+        borderTopWidth: 7,
+        borderTopRightRadius: 7,
+        borderTopLeftRadius: 7,
+        width: '40%',
+        height: '40%',
     }
 });
 
